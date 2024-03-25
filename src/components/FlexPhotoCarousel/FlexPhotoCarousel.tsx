60e51ece3ac1b67bcdaeecd2c1937ce6c8187e4f
Karl Horning <karl.horning@learnlight.com>
2024-03-25 22:35:45 +0000

    Add code to animate slide transitions

--- src/components/FlexPhotoCarousel/FlexPhotoCarousel.tsx
+++ src/components/FlexPhotoCarousel/FlexPhotoCarousel.tsx
@@ -31,30 +31,34 @@ export default function FlexPhotoCarousel({
     };
 
     const showNextSlide = () => {
-        setSlideIndex((index) =>
-            index === slides.length - 1 ? 0 : index + 1
-        );
+        setSlideIndex((index) => (index === slides.length - 1 ? 0 : index + 1));
     };
 
     return (
         <div className="w-full h-full relative">
-            <Image
-                className="w-full h-full object-cover block box-border"
-                height={780}
-                width={1400}
-                layout="responsive"
-                src={slides[slideIndex].url}
-                alt=""
-            />
+            <div className="w-full h-full flex" style={{ overflow: "hidden" }}>
+                {slides.map((slide) => (
+                    <Image
+                        className="w-full h-full object-cover block box-border flex-shrink-0 flex-grow-0 transition-all delay-300 ease-in-out"
+                        height={800}
+                        width={1400}
+                        layout="responsive"
+                        src={slide.url}
+                        alt=""
+                        key={slide.url}
+                        style={{ translate: `${-100 * slideIndex}%` }}
+                    />
+                ))}
+            </div>
             <button
                 onClick={showPrevSlide}
-                className="block absolute top-0 bottom-0 p-4 cursor-pointer left-0 hover:bg-black/10 transition-colors duration-100 ease-in-out"
+                className="slide-button block absolute top-0 bottom-0 p-4 cursor-pointer left-0 hover:bg-black/10 transition-colors duration-100 ease-in-out"
             >
                 <LuArrowBigLeft className="fill-black stroke-white h-8 w-8" />
             </button>
             <button
                 onClick={showNextSlide}
-                className="block absolute top-0 bottom-0 p-4 cursor-pointer right-0 hover:bg-black/10 transition-colors duration-100 ease-in-out"
+                className="slide-button block absolute top-0 bottom-0 p-4 cursor-pointer right-0 hover:bg-black/10 transition-colors duration-100 ease-in-out"
             >
                 <LuArrowBigRight className="fill-black stroke-white h-8 w-8" />
             </button>
