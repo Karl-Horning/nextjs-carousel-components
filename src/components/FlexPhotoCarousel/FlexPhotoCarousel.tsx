a89b1e685d712fe45fc3b3eea50df5df6bd9766d
Karl Horning <karl.horning@learnlight.com>
2024-03-26 00:14:16 +0000

    Add accessibility labels and styles

--- src/components/FlexPhotoCarousel/FlexPhotoCarousel.tsx
+++ src/components/FlexPhotoCarousel/FlexPhotoCarousel.tsx
@@ -2,7 +2,12 @@
 
 import Image from "next/image";
 import { useState } from "react";
-import { LuArrowBigLeft, LuArrowBigRight } from "react-icons/lu";
+import {
+    LuArrowBigLeft,
+    LuArrowBigRight,
+    LuCircle,
+    LuCircleDot,
+} from "react-icons/lu";
 import style from "./FlexPhotoCarousel.module.css";
 
 /**
@@ -36,21 +41,26 @@ export default function FlexPhotoCarousel({
     };
 
     return (
-        <div id="flex-photo-carousel" className="w-full h-full relative">
+        <section
+            id="flex-photo-carousel"
+            aria-label="Photo Carousel"
+            className="w-full h-full relative"
+        >
             <div
                 id="slide-container"
                 className="w-full h-full flex"
                 style={{ overflow: "hidden" }}
             >
-                {slides.map((slide) => (
+                {slides.map(({ url, alt }, index) => (
                     <Image
                         className="w-full h-full object-cover block box-border flex-shrink-0 flex-grow-0 transition-all delay-300 ease-in-out"
                         height={800}
                         width={1400}
                         layout="responsive"
-                        src={slide.url}
-                        alt={slide.alt || ""}
-                        key={slide.url}
+                        src={url}
+                        alt={alt || ""}
+                        aria-hidden={slideIndex !== index}
+                        key={url}
                         style={{ translate: `${-100 * slideIndex}%` }}
                     />
                 ))}
@@ -58,17 +68,50 @@ export default function FlexPhotoCarousel({
             <button
                 id="prev-button"
                 onClick={showPrevSlide}
-                className={`${style["slide-button"]} block absolute top-0 bottom-0 p-4 cursor-pointer left-0 hover:bg-black/10 transition-colors duration-100 ease-in-out`}
+                className={`${style["slide-button"]} block absolute top-0 bottom-0 p-4 cursor-pointer left-0 hover:bg-black/10 focus-visible:bg-black/10 transition-colors duration-100 ease-in-out`}
+                aria-label="View previous slide"
             >
-                <LuArrowBigLeft className="fill-black stroke-white h-8 w-8" />
+                <LuArrowBigLeft
+                    aria-hidden
+                    className="fill-black stroke-white h-8 w-8"
+                />
             </button>
             <button
                 id="next-button"
                 onClick={showNextSlide}
-                className={`${style["slide-button"]} block absolute top-0 bottom-0 p-4 cursor-pointer right-0 hover:bg-black/10 transition-colors duration-100 ease-in-out`}
+                className={`${style["slide-button"]} block absolute top-0 bottom-0 p-4 cursor-pointer right-0 hover:bg-black/10 focus-visible:bg-black/10 transition-colors duration-100 ease-in-out`}
+                aria-label="View next slide"
             >
-                <LuArrowBigRight className="fill-black stroke-white h-8 w-8" />
+                <LuArrowBigRight
+                    aria-hidden
+                    className="fill-black stroke-white h-8 w-8"
+                />
             </button>
-        </div>
+            <div
+                id="slide-navigation"
+                className="absolute bottom-2 left-[50%] translate-x-[-50%] translate-y-[-50%] flex gap-1"
+            >
+                {slides.map((_, index) => (
+                    <button
+                        className={`${style["slide-pagination"]} h-4 w-4 block`}
+                        onClick={() => setSlideIndex(index)}
+                        key={index}
+                        aria-label={`View slide ${index + 1}`}
+                    >
+                        {index === slideIndex ? (
+                            <LuCircleDot
+                                aria-hidden
+                                className="stroke-white fill-black h-full w-full"
+                            />
+                        ) : (
+                            <LuCircle
+                                aria-hidden
+                                className="stroke-white fill-black h-full w-full"
+                            />
+                        )}
+                    </button>
+                ))}
+            </div>
+        </section>
     );
 }
